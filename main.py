f62c50549e5aa024f537bfec0666df934fcc98cb
vinnypinhosantos <vinnypinhosantos@gmail.com>
2022-09-22 20:53:42 -0300

    Making the Mad Libs

---
+++ main.py
@@ -0,0 +1,23 @@
+adj1 = input("Adjective: ")
+adj2 = input("Adjective: ")
+type_of_bird = input("Type of bird: ")
+room_house = input("Room of a house: ")
+verb_past = input("Verb (past_tense: ")
+verb = input("Verb: ")
+relative_name = input("Relative's name: ")
+noun = input("Noun: ")
+liquid = input("Liquid: ")
+verb_ing = input("Verb ending in ing: ")
+part_body = input("Part of the Body (in plural): ")
+plural_noun = input("Noun in plural: ")
+verb_ing2 = input("Verb ending in ing: ")
+noun2 = input("Noun: ")
+
+mad_libs = f'It was a {adj1}, cold November day. I woke up to the {adj2} smell of {type_of_bird} ' \
+           f'roasting in the {room_house} downstairs. I {verb_past} down the stairs to see if I ' \
+           f'could help {verb} the dinner. My mom said, "See if {relative_name} needs a fresh ' \
+           f'{noun}." So I carried a tray of glasses full of {liquid} into the {verb_ing} ' \
+           f'room. When I got there, I coundt believe my {part_body}! There were {plural_noun} ' \
+           f'{verb_ing2} on the {noun2}'
+
+print(mad_libs)
